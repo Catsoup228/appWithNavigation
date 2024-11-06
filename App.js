6f0e7b570f27b7bdc98ecc42ce707973d8c8d061
Catsoup228 <kafyant@gmail.com>
2024-11-06 15:30:45 +0300

    add navigation

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AboutMe from './components/greeting';
+import MyLanguages from './components/languages';
+import NotFound from './components/notFound';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Tab = createBottomTabNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="О Себе" component={AboutMe} />
+                <Tab.Screen name="Мои языки программирования" component={MyLanguages} />
+                <Tab.Screen name="Не найдено" component={NotFound} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
---
+++ components/languages.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, FlatList, SafeAreaView  } from 'react-native';
+import React from 'react';
+import LanguageItem from './languageItems';
+
+export default function App() {
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FlatList
+        data={langs}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <LanguageItem logo={item.logo} lang={item.lang} experience={item.experience} />
+        )}
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+      />
+    </SafeAreaView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const langs = [
+  { id: '1', lang: 'JavaScript', logo: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png', experience: 3 },
+  { id: '2', lang: 'Python', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png', experience: 1 },
+]
